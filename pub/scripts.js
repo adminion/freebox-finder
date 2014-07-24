162a655dc16eb199735e6f0ed876308c8a09490c
Jeff Harris <techjeffharris@gmail.com>
2014-07-24 13:57:14 -0700

    Deconstructed the callback pyramid

--- pub/scripts.js
+++ pub/scripts.js
@@ -28,22 +28,31 @@ $(document).ready(function () {
     socket = io();
 
     socket.on('connect', socketConnect )
+
 });
 
 function socketConnect () {
 
+    getLocation();
+
+}
+
+function getLocation () {
     myMap = new Map(document.getElementById('map-canvas'), { center: config.center, zoom: config.zoom });
     console.log('myMap', myMap);
 
     // check for navigator
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getCurrentPositionResults) ;
+
+        console.log('navigator supported!');
+        navigator.geolocation.getCurrentPosition(getCurrentPositionResults, getCurrentPositionError);
+
+    } else {
+        console.log('navigator NOT supported!');
     }
-
-
-
 }
+
 
 function getCurrentPositionResults (position) {
             
@@ -57,47 +66,86 @@ function getCurrentPositionResults (position) {
     
     $('#current-location .container').replaceWith(newLocationCoords)
 
+    getGeocodedLocation(position)
+}
+
+function getCurrentPositionError(error) {
+
+    var location = document.getElementById('location'),
+        msg;
+
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            var msg = 'User denied the request for Geolocation.'
+            break;
+        case error.POSITION_UNAVAILABLE:
+            var msg = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            var msg = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            var msg = "An unknown error occurred."
+            break;
+    }
+
+    $('#current-location .container').replaceWith("<div class='container'>" + msg + " </div>")
+    console.log(msg)
+            
+}
+
+function getGeocodedLocation (position) {
+
     var currentLatLng = new LatLng(position.coords.latitude, position.coords.longitude);
+    console.log('currentLatLng', currentLatLng)
    
     if (myGeocoder) {
         myGeocoder.geocode(currentLatLng, geocodeResults);
     }
+
 }
 
 function geocodeResults (results, status) {
 
     if (status == google.maps.GeocoderStatus.OK) {
 
-        var result = 0
-
         console.log('results[' + result + ']', results[result])
 
-        myMap.fitBounds(results[result].geometry.viewport);
+        updateMap(results[0]) 
+    } else {
+        console.log('status', status)
+    }
+}
 
-        var boxes = [
-            { title: '1218 Mall Street', content: "This is where Zane lives.", position: ZanesHouse, map: myMap },
-            { title: 'freebox 1', content: "This is freebox 1", position: new LatLng(45.699667, -121.537079), map: myMap },
-            { title: 'freebox 2', content: "This is freebox 2", position: new LatLng(45.705001, -121.522402), map: myMap }
-        ];
+function updateMap (results) {
 
-        for (var index in boxes) {
+    myMap.fitBounds(results[result].geometry.viewport);
 
-            var box = boxes[index];
+    var boxes = [
+        { title: '1218 Mall Street', content: "This is where Zane lives.", position: ZanesHouse, map: myMap },
+        { title: 'freebox 1', content: "This is freebox 1", position: new LatLng(45.699667, -121.537079), map: myMap },
+        { title: 'freebox 2', content: "This is freebox 2", position: new LatLng(45.705001, -121.522402), map: myMap }
+    ];
 
-            cache.markers.push(new Marker(box));
+    for (var index in boxes) {
+
+        var box = boxes[index];
+
+        cache.markers.push(new Marker(box));
 
 // 
 //      trying to get this to work:
 // 
 
-            cache.infoWindows.push(new InfoWindow({ 
-                content: box.content,
-                position: box.position
-            }));
-            
-            cache.infoWindows.slice(-1,-1).open(myMap);
-        }
-
-        console.log('cache',cache)
+        cache.infoWindows.push(new InfoWindow({ 
+            content: box.content,
+            position: box.position
+        }));
     }
+
+    for (var index in cache.infoWindows) {
+        cache.infoWindows[index].open(myMap);     
+    }
+
+    console.log('cache',cache)
 }
