324a3e9be662d2d71a6c1307ff232fc361c41170
Jeff Harris <techjeffharris@gmail.com>
2014-08-20 22:16:35 -0700

    Update setup.sh
    removed sudo -- sudo will automatically prompt for password, npm should be run as normal user.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 
-apt-get update && sudo apt-get -y install gcc make build-essential nodejs npm mongodb
+apt-get update && apt-get -y install gcc make build-essential nodejs npm mongodb
 
 npm install
