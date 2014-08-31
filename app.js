c22f9b456c72348040bc527b045d0e357f3ff2fa
Jeff Harris <techjeffharris@gmail.com>
2014-08-30 21:09:18 -0700

    v0.2.0-beta

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var FreeboxFinder = require('./lib/'),
     config = require('./lib/config'),
     env = require('./lib/env'),
     interrupt,
-    server;
+    server,
+    stopping;
 
 process.on('SIGINT', function () {
-    if (interrupt) {
+    if (interrupt && stopping) {
         server.stop(process.exit);
     } else {
         console.log('\n(^C again to quit)'); 
+        stopping = true;
         interrupt = setTimeout(function () {
             interrupt = undefined;
         }, 1000);
